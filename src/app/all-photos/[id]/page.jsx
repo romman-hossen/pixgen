2d95b1061f5b3e573dd78a7cf364be6ac3602867
romman-hossen <mdromman417@gmail.com>
2026-06-20 01:19:58 +0600

    Fixed env URI

--- src/app/all-photos/[id]/page.jsx
+++ src/app/all-photos/[id]/page.jsx
@@ -2,18 +2,18 @@ import { GetData } from "@/lib/data";
 
 const PhotosDataDetails = async({params}) => {
     const {id} = await params;
+    console.log(id)
     const data = await GetData();
+    console.log(data)
 
-    const findData = data.find(d=> d.id === Number(id));
-    // console.log(findData)
+    const findData = data.find(d => d.id == id);
+    console.log(findData)
     
     // console.log(typeof id)
     return (
         <div className="max-w-7xl mx-auto my-10">
             <h2 className="text-3xl font-bold">This is Details Section</h2>
             <h3>{findData.prompt}</h3>
-            
-            
         </div>
     );
 };
